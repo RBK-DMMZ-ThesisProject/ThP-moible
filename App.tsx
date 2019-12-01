d5372d7e71f242d49059bc32ec85d6b8372ec391
znha <zihna-19@hotmail.com>
2019-12-01 15:20:45 +0200

    creating the home screen

--- App.tsx
+++ App.tsx
@@ -14,68 +14,43 @@ import {
   StyleSheet,
   ScrollView,
   View,
+  Button,
   Text,
+  Image,
   StatusBar,
+  Alert,
 } from 'react-native';
 
 import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
+  Colors
 } from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
-  const usingHermes = typeof HermesInternal === 'object' && HermesInternal !== null;
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {!usingHermes ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+      <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center' }}>
+        <Image source={require('./assets/shaghelohm.png')} style={{ flex: 5, width: 90 + "%", height: 200 }} />
+        <Text style={{ flex: 1, color: "#91cde0", fontFamily: 'Cochin', fontSize: 36, fontWeight: 'bold' }}>Handy</Text>
+        <View style={{ flex: 2 }}>
+          <Button
+            title="Discover"
+            color="#63b8d4"
+            onPress={() => Alert.alert('Discover pressed')}
+          /></View>
+      </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
+  baseText: {
+    fontFamily: 'Cochin',
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
   scrollView: {
     backgroundColor: Colors.lighter,
   },
