19671e1fc53e054d6c85e83175d3cc5551bdde79
mahanbahin <maha.a.nbahin@hotmail.com>
2019-12-14 17:59:42 +0200

    chnges

--- App.tsx
+++ App.tsx
@@ -9,9 +9,9 @@
  */
 import React from 'react';
 
-import { Icon } from 'native-base';
-import { createAppContainer } from 'react-navigation';
-import { createDrawerNavigator } from 'react-navigation-drawer';
+import {Icon} from 'native-base';
+import {createAppContainer} from 'react-navigation';
+import {createDrawerNavigator} from 'react-navigation-drawer';
 // import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from './components/HomeScreen';
 import CategoriesScreen from './components/CategoriesScreen';
@@ -22,9 +22,11 @@ import serviceProviderProfile from './components/serviceProviderProfile';
 import chatt from './components/chattingApp/chatt';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
-import { DrawerNavigatorItems } from 'react-navigation-drawer';
+import Favorites from './components/Favorites';
+import HireHistory from './components/HireHistory';
+import {DrawerNavigatorItems} from 'react-navigation-drawer';
 import SafeAreaView from 'react-native-safe-area-view';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import store from './state/store';
 import CustomHamburgerMenuDrawer from './components/HamburgerMenuDrawer';
 import NavigationService from './components/NavigationService';
@@ -38,90 +40,105 @@ import NavigationService from './components/NavigationService';
 import {
   NavigationParams,
   NavigationScreenProp,
-  NavigationState
-
+  NavigationState,
 } from 'react-navigation';
 export interface Props {
   navigation: NavigationScreenProp<NavigationState, NavigationParams>;
 }
-const Drawer = createDrawerNavigator({
-  Home: {
-    screen: HomeScreen,
-    navigationOptions: {
-      title: 'Homepage',
+const Drawer = createDrawerNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        title: 'Homepage',
+      },
+    },
+    Categories: {
+      screen: CategoriesScreen,
+      navigationOptions: {
+        title: 'Categories',
+      },
+    },
+    ProfilesScreen: {
+      screen: ProfilesScreen,
+      navigationOptions: {
+        title: 'Profiles',
+      },
+    },
+    AddProfileScreen: {
+      screen: AddProfileScreen,
+      navigationOptions: {
+        title: 'Add Profile',
+      },
+    },
+    ViewProfile: {
+      screen: ViewProfileScreen,
+      navigationOptions: {
+        title: 'Profile',
+      },
+    },
+    ProviderProfile: {
+      screen: serviceProviderProfile,
+      navigationOptions: {
+        title: 'ProviderProfile',
+      },
+    },
+    chattScreen: {
+      screen: chatt,
+      navigationOptions: {
+        title: 'chattScreen',
+      },
+    },
+    SignIn: {
+      screen: SignIn,
+      navigationOptions: {
+        title: 'Sign in',
+      },
+    },
+    SignUp: {
+      screen: SignUp,
+      navigationOptions: {
+        title: 'Sign up',
+      },
+    },
+    CustomHamburgerMenuDrawer: {
+      screen: CustomHamburgerMenuDrawer,
+      navigationOptions: {
+        title: 'Custom menu',
+      },
+    },
+    Favorites: {
+      screen: Favorites,
+      navigationOptions: {
+        title: 'Favorites',
+      },
+    },
+    HireHistory: {
+      screen: HireHistory,
+      navigationOptions: {
+        title: 'HireHistory',
+      },
     },
   },
-  Categories: {
-    screen: CategoriesScreen,
-    navigationOptions: {
-      title: 'Categories',
-    },
-  },
-  ProfilesScreen: {
-    screen: ProfilesScreen,
-    navigationOptions: {
-      title: 'Profiles',
-    },
-  },
-  AddProfileScreen: {
-    screen: AddProfileScreen,
-    navigationOptions: {
-      title: 'Add Profile',
-    },
-  },
-  ViewProfile: {
-    screen: ViewProfileScreen,
-    navigationOptions: {
-      title: 'Profile',
-    },
-  },
-  ProviderProfile: {
-    screen: serviceProviderProfile,
-    navigationOptions: {
-      title: 'ProviderProfile',
-    },
-  },
-  chattScreen: {
-    screen: chatt,
-    navigationOptions: {
-      title: 'chattScreen',
-    },
-  },
-  SignIn: {
-    screen: SignIn,
-    navigationOptions: {
-      title: 'Sign in',
-    },
-  },
-  SignUp: {
-    screen: SignUp,
-    navigationOptions: {
-      title: 'Sign up',
-    },
-  },
-  CustomHamburgerMenuDrawer: {
-    screen: CustomHamburgerMenuDrawer,
-    navigationOptions: {
-      title: 'Custom menu'
-    }
-  }
-},
 
   {
     contentComponent: () => {
-
-      return <CustomHamburgerMenuDrawer />
+      return <CustomHamburgerMenuDrawer />;
     },
-
-  });
+  },
+);
 
 const AppContainer = createAppContainer(Drawer);
 export default class App extends React.Component<Props> {
   render() {
-    return (<Provider store={store}>
-      <AppContainer ref={(navigatorRef: any) => {
-        NavigationService.setTopLevelNavigator(navigatorRef);
-      }} />
-    </Provider>);
+    return (
+      <Provider store={store}>
+        <AppContainer
+          ref={(navigatorRef: any) => {
+            NavigationService.setTopLevelNavigator(navigatorRef);
+          }}
+        />
+      </Provider>
+    );
   }
 }
---
+++ components/Favorites.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import {View, TouchableOpacity, Alert} from 'react-native';
+import {Card, ListItem, Button, Icon, Rating} from 'react-native-elements';
+import {
+  NavigationParams,
+  NavigationScreenProp,
+  NavigationState,
+} from 'react-navigation';
+import HandyHeader from './HandyHeader';
+// import {connect} from 'react-redux';
+// import * as types from '../state/types';
+// import {Dispatch} from 'react-redux';
+export interface Props {
+  navigation: NavigationScreenProp<NavigationState, NavigationParams>;
+}
+class Favorites extends React.Component<Props, object> {
+  state = {
+    favorites: [],
+  };
+  componentDidMount() {
+    var that = this;
+    var SharedPreferences = require('react-native-shared-preferences');
+    SharedPreferences.setName('handyInfo');
+    SharedPreferences.getItem('handyToken', function(value: any) {
+      if (value === null) {
+        console.log('no token');
+        that.props.navigation.navigate('SignIn');
+      } else {
+        fetch('https://salty-garden-58258.herokuapp.com/mobileApi/favorites', {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({customerID: value}),
+        })
+          .then(res => res.json())
+          .then(resJson => {
+            console.log('response: ', resJson);
+            that.setState({
+              favorites: resJson,
+            });
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      }
+    });
+  }
+
+  render() {
+    const {favorites} = this.state;
+    const {navigation} = this.props;
+    return (
+      <>
+        <HandyHeader navigation={navigation} title={'Favorites'} />
+        <Card containerStyle={{padding: 5}}>
+          {console.log('favorites', favorites)}
+          {favorites.map((provider, i) => {
+            return (
+              <View key={i}>
+                <TouchableOpacity
+                  onPress={() =>
+                    navigation.navigate('ProviderProfile', {
+                      userId: provider['_id'],
+                    })
+                  }>
+                  <ListItem
+                    leftAvatar={{
+                      title: provider['userName'][0],
+                      source: {uri: provider['userImg']},
+                    }}
+                    title={provider['userName']}
+                    chevron></ListItem>
+                </TouchableOpacity>
+              </View>
+            );
+          })}
+        </Card>
+      </>
+    );
+  }
+}
+
+export default Favorites;
