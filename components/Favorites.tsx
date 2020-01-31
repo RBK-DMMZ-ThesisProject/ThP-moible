730bfd1750d6b3cf48181ec427e7ea4d559b0ad6
zinah aroqi <zihna-19@hotmail.com>
2020-01-31 11:32:22 +0200

    fixing favorites file'

--- components/Favorites.tsx
+++ components/Favorites.tsx
@@ -1,27 +1,21 @@
 import React from 'react';
-import { View, TouchableOpacity, Alert, StyleSheet, ActivityIndicator } from 'react-native';
 import {
-  Card,
-  ListItem,
-  Button,
-  Icon,
-  Rating,
-  Text,
-} from 'react-native-elements';
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  ActivityIndicator,
+} from 'react-native';
+import {Card, ListItem, Text} from 'react-native-elements';
 import {
   NavigationParams,
   NavigationScreenProp,
   NavigationState,
 } from 'react-navigation';
-import { connect } from 'react-redux';
-import { Dispatch } from 'react-redux';
-import {
-  changeActivityIndicatorState,
-} from '../state/actions';
+import {connect} from 'react-redux';
+import {Dispatch} from 'react-redux';
+import {changeActivityIndicatorState} from '../state/actions';
 import HandyHeader from './HandyHeader';
-// import {connect} from 'react-redux';
-// import * as types from '../state/types';
-// import {Dispatch} from 'react-redux';
+
 export interface Props {
   navigation: NavigationScreenProp<NavigationState, NavigationParams>;
   activityIndicatorState: boolean;
@@ -31,37 +25,42 @@ class Favorites extends React.Component<Props, object> {
   state = {
     favorites: [],
   };
+
   constructor(props) {
     super(props);
     this.props.changeActivityIndicatorState(true);
   }
+
   componentDidMount() {
-    var that = this;
     this.props.changeActivityIndicatorState(true);
+
     var SharedPreferences = require('react-native-shared-preferences');
     SharedPreferences.setName('handyInfo');
-    SharedPreferences.getItem('handyToken', async function (value: any) {
+    SharedPreferences.getItem('handyToken', async (value: any) => {
       if (value === null) {
-        console.log('no token');
-        that.props.navigation.navigate('SignIn');
+        this.props.navigation.navigate('SignIn');
       } else {
-        await fetch('https://salty-garden-58258.herokuapp.com/mobileApi/favorites', {
-          method: 'POST',
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
+        // fetch favorites from database
+        await fetch(
+          'https://salty-garden-58258.herokuapp.com/mobileApi/favorites',
+          {
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({customerID: value}),
           },
-          body: JSON.stringify({ customerID: value }),
-        })
-          .then(res => res.json())
-          .then(resJson => {
-            that.props.changeActivityIndicatorState(false);
-            that.setState({
+        )
+          .then((res: any) => res.json())
+          .then((resJson: any) => {
+            this.props.changeActivityIndicatorState(false);
+            this.setState({
               favorites: resJson.favorites,
             });
           })
-          .catch(error => {
-            that.props.changeActivityIndicatorState(false);
+          .catch((error: any) => {
+            this.props.changeActivityIndicatorState(false);
             console.error(error);
           });
       }
@@ -69,16 +68,16 @@ class Favorites extends React.Component<Props, object> {
   }
 
   render() {
-    const { favorites } = this.state;
-    const { navigation } = this.props;
+    const {favorites} = this.state;
+    const {navigation} = this.props;
+
     return (
       <>
         <HandyHeader navigation={navigation} title={'Favorites'} />
         {favorites.map((provider, i) => {
-          console.log('prodived id in favs**********', provider)
           return (
-            <Card containerStyle={{ padding: 5 }} key={i}>
-              <View >
+            <Card containerStyle={{padding: 5}} key={i}>
+              <View>
                 <TouchableOpacity
                   onPress={() =>
                     navigation.navigate('ProviderProfile', {
@@ -88,7 +87,7 @@ class Favorites extends React.Component<Props, object> {
                   <ListItem
                     leftAvatar={{
                       title: provider['userName'][0],
-                      source: { uri: provider['userImg'] },
+                      source: {uri: provider['userImg']},
                     }}
                     title={provider['userName']}
                     subtitle={provider['ServiceCategory']}
@@ -103,30 +102,29 @@ class Favorites extends React.Component<Props, object> {
             <ActivityIndicator size="large" color="#c5df16" />
           </View>
         ) : (
-            <Text></Text>
-          )}
+          <Text></Text>
+        )}
       </>
     );
   }
 }
-//@Description: pass functions as props of the component
-const mapDsipatchToProps = (dispatch: Dispatch) => ({
 
+//@function: mapDispatchToProps
+//@Description: Pass functions as props of the component
+const mapDispatchToProps = (dispatch: Dispatch) => ({
   changeActivityIndicatorState: (state: boolean) => {
     dispatch(changeActivityIndicatorState(state));
   },
 });
 
-//@Description: pass propereties as props of the component
-const mapStateToProps = (
-  appstate: any,
-) => {
+//@function: mapStateToProps
+//@Description: Pass properties as props of the component
+const mapStateToProps = (appState: any) => {
   return {
-
-    activityIndicatorState: appstate.changeGeneralState.activityIndicatorState,
-
+    activityIndicatorState: appState.changeGeneralState.activityIndicatorState,
   };
 };
+
 //@Description: style the activity indicator
 const styles = StyleSheet.create({
   loading: {
@@ -139,7 +137,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-export default connect(
-  mapStateToProps,
-  mapDsipatchToProps,
-)(Favorites);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
