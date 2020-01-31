fb39856ad807f380bf053e74f3940fc79093e5e7
zinah aroqi <zihna-19@hotmail.com>
2020-01-31 16:38:58 +0200

    deleting offers file andfixing profiles screen file'

--- components/ProfilesScreen.tsx
+++ components/ProfilesScreen.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { View, TouchableOpacity, Alert, StyleSheet, ActivityIndicator, Text } from 'react-native';
-import { Card, ListItem, Button, Icon, Rating } from 'react-native-elements';
-
-import { connect } from 'react-redux';
-import { Dispatch } from 'react-redux';
 import {
-  changeActivityIndicatorState,
-} from '../state/actions';
-
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  ActivityIndicator,
+  Text,
+} from 'react-native';
+import {Card, ListItem, Button, Icon, Rating} from 'react-native-elements';
+import {connect} from 'react-redux';
+import {Dispatch} from 'react-redux';
+import {changeActivityIndicatorState} from '../state/actions';
 import {
   NavigationParams,
   NavigationScreenProp,
@@ -18,7 +20,6 @@ import NavigationService from './NavigationService.js';
 
 export interface Props {
   navigation: NavigationScreenProp<NavigationState, NavigationParams>;
-
   activityIndicatorState: boolean;
   changeActivityIndicatorState: any;
 }
@@ -32,67 +33,46 @@ class ProfilesScreen extends React.Component<Props, object> {
     super(props);
     this.props.changeActivityIndicatorState(true);
   }
+
   componentDidMount() {
     const categoryName = this.props.navigation.getParam('categoryName');
-    const { profiles } = this.state;
+    const {profiles} = this.state;
     this.props.changeActivityIndicatorState(true);
-    var that = this;
     fetch('https://salty-garden-58258.herokuapp.com/mobileApi/getProfiles', {
       method: 'POST',
       headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
       },
 
-      body: JSON.stringify({ ServiceCategory: categoryName }),
+      body: JSON.stringify({ServiceCategory: categoryName}),
     })
-      .then(res => res.json())
-      .then(resJson => {
-        console.log('response: ', resJson);
+      .then((res:any) => res.json())
+      .then((resJson:any) => {
         this.setState({
           profiles: resJson.profil,
           rating: resJson.rates,
         });
-        that.props.changeActivityIndicatorState(false);
+        this.props.changeActivityIndicatorState(false);
       })
-      .catch(error => {
+      .catch((error:any) => {
         console.error(error);
-        that.props.changeActivityIndicatorState(false);
+        this.props.changeActivityIndicatorState(false);
       });
-    // fetch('https://salty-garden-58258.herokuapp.com/mobileApi/getRate', {
-    //   method: 'POST',
-    //   headers: {
-    //     Accept: 'application/json',
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify({serviceproviderid: profiles.}),
-
-    // })
-    //   .then(res => res.json())
-    //   .then(resJson => {
-    //     console.log('response: ', resJson);
-    //     this.setState({
-    //       rating: resJson,
-    //     });
-    //   })
-    //   .catch(error => {
-    //     console.error(error);
-    //   });
   }
 
   render() {
-    const { profiles } = this.state;
-    const { navigation } = this.props;
-    // const {rating} = this.props;
-    const { rating } = this.state;
-    const categoryName = this.props.navigation.getParam('categoryName');
+
+    const {navigation} = this.props;
+    const {rating, profiles} = this.state;
+    const categoryName = navigation.getParam('categoryName');
 
     return (
       <>
         <HandyHeader navigation={navigation} title={categoryName} />
         {profiles.map((user: any, i: any) => {
           return (
-            <Card containerStyle={{ padding: 5 }} key={i}>
+            <Card containerStyle={{padding: 5}} key={i}>
               <View>
                 <TouchableOpacity
                   onPress={() =>
@@ -103,13 +83,13 @@ class ProfilesScreen extends React.Component<Props, object> {
                   <ListItem
                     leftAvatar={{
                       title: 'SP',
-                      source: { uri: user['userImg'] },
+                      source: {uri: user['userImg']},
                     }}
-                    titleStyle={{ position: 'absolute', top: -20, left: -4 }}
+                    titleStyle={{position: 'absolute', top: -20, left: -4}}
                     title={user['userName']}
                     chevron></ListItem>
                 </TouchableOpacity>
-                <View style={{ position: 'absolute', top: 40, left: 65 }}>
+                <View style={{position: 'absolute', top: 40, left: 65}}>
                   <Rating
                     type="custom"
                     ratingColor="#078ca9"
@@ -129,32 +109,29 @@ class ProfilesScreen extends React.Component<Props, object> {
             <ActivityIndicator size="large" color="#c5df16" />
           </View>
         ) : (
-            <Text></Text>
-          )}
+          <Text></Text>
+        )}
       </>
     );
   }
 }
 
-
-//@Description: pass functions as props of the component
-const mapDsipatchToProps = (dispatch: Dispatch) => ({
-
+//@function: mapDispatchToProps
+//@description: pass functions as props of the component
+const mapDispatchToProps = (dispatch: Dispatch) => ({
   changeActivityIndicatorState: (state: boolean) => {
     dispatch(changeActivityIndicatorState(state));
   },
 });
 
-//@Description: pass propereties as props of the component
-const mapStateToProps = (
-  appstate: any,
-) => {
+//@function: mapStateToProps
+//@Description: pass properties as props of the component
+const mapStateToProps = (appState: any) => {
   return {
-
-    activityIndicatorState: appstate.changeGeneralState.activityIndicatorState,
-
+    activityIndicatorState: appState.changeGeneralState.activityIndicatorState,
   };
 };
+
 //@Description: style the activity indicator
 const styles = StyleSheet.create({
   loading: {
@@ -167,7 +144,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-export default connect(
-  mapStateToProps,
-  mapDsipatchToProps,
-)(ProfilesScreen);
+export default connect(mapStateToProps, mapDispatchToProps)(ProfilesScreen);
