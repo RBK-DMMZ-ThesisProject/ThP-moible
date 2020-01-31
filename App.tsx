bd6c1f89cc715cacc0b455deba3a140591f69d56
zinah aroqi <zihna-19@hotmail.com>
2020-01-31 17:49:23 +0200

    fixing main app file

--- App.tsx
+++ App.tsx
@@ -8,8 +8,6 @@
  * @format
  */
 import React from 'react';
-import {View, Button} from 'react-native';
-import {Icon} from 'native-base';
 import {createAppContainer} from 'react-navigation';
 import {createDrawerNavigator} from 'react-navigation-drawer';
 import {createStackNavigator} from 'react-navigation-stack';
@@ -22,29 +20,22 @@ import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import Favorites from './components/Favorites';
 import HireHistory from './components/HireHistory';
-import {DrawerNavigatorItems} from 'react-navigation-drawer';
-import SafeAreaView from 'react-native-safe-area-view';
 import {Provider} from 'react-redux';
 import store from './state/store';
 import CustomHamburgerMenuDrawer from './components/HamburgerMenuDrawer';
 import NavigationService from './components/NavigationService';
 import serviceProviderProfile from './components/serviceProviderProfile';
 import Chatt from './components/chattingApp/chatt';
-// import Offers from './components/Offers';
-// const MainNavigator = createStackNavigator({
-//   Home: { screen: HomeScreen },
-//   Categories: { screen: CategoriesScreen },
-//   ProfilesScreen: { screen: ProfilesScreen },
-// });
-// const App = createAppContainer(MainNavigator);
 import {
   NavigationParams,
   NavigationScreenProp,
   NavigationState,
 } from 'react-navigation';
+
 export interface Props {
   navigation: NavigationScreenProp<NavigationState, NavigationParams>;
 }
+// main navigation stack
 export const MainStack = createStackNavigator(
   {
     Home: {
@@ -107,81 +98,19 @@ export const MainStack = createStackNavigator(
         title: 'Favorites',
       },
     },
-    HireHistory: {
-      screen: HireHistory,
-      navigationOptions: {
-        title: 'Hire History',
-      },
-    },
-    // Offers: {
-    //   screen: Offers,
-    //   navigationOptions: {
-    //     title: 'Offers',
-    //   },
-    // }
   },
   {headerMode: 'none'},
 );
-
+// The drawer navigation stack with the main navigation stack and a custom compoenent
 export const Drawer = createDrawerNavigator(
   {
-    // Home: {
-    //   screen: HomeScreen,
-    //   navigationOptions: {
-    //     title: 'Homepage',
-
-    //   }
-    // },
-    // Categories: {
-    //   screen: CategoriesScreen,
-    //   navigationOptions: {
-    //     title: 'Categories'
-    //   }
-    // },
-    // ProfilesScreen: {
-    //   screen: ProfilesScreen,
-    //   navigationOptions: {
-    //     title: 'Profiles'
-    //   }
-    // },
-    // AddProfileScreen: {
-    //   screen: AddProfileScreen,
-    //   navigationOptions: {
-    //     title: 'Add Profile'
-    //   }
-    // },
-    // ViewProfile: {
-    //   screen: ViewProfileScreen,
-    //   navigationOptions: {
-    //     title: 'Profile'
-    //   }
-    // },
-    // ProviderProfile: {
-    //   screen: serviceProviderProfile,
-    //   navigationOptions: {
-    //     title: 'Profile'
-    //   }
-    // },
-    // SignIn: {
-    //   screen: SignIn,
-    //   navigationOptions: {
-    //     title: 'Sign in'
-    //   }
-    // },
-    // SignUp: {
-    //   screen: SignUp,
-    //   navigationOptions: {
-    //     title: 'Sign up'
-    //   }
-    // },
     MainStack: {
       screen: MainStack,
     },
   },
-
   {
     contentComponent: () => {
-      return <CustomHamburgerMenuDrawer key={2156} />;
+      return <CustomHamburgerMenuDrawer  />;
     },
     contentOptions: {
       activeTintColor: '#666666',
@@ -195,9 +124,6 @@ export const Drawer = createDrawerNavigator(
 );
 export const AppStack = createAppContainer(Drawer);
 
-// In Your App.js
-
-// const AppContainer = createAppContainer(Drawer);
 export default class App extends React.Component<Props> {
   render() {
     return (
