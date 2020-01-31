11f2263b123e2eb9b8001b5f2ed8255a16b16d74
zinah aroqi <zihna-19@hotmail.com>
2020-01-31 11:47:24 +0200

    fixing header file

--- components/HandyHeader.tsx
+++ components/HandyHeader.tsx
@@ -1,31 +1,40 @@
-import React from "react";
-import { Header } from "react-native-elements";
+import React from 'react';
+import {Header} from 'react-native-elements';
 import {
-    NavigationParams,
-    NavigationScreenProp,
-    NavigationState
-
+  NavigationParams,
+  NavigationScreenProp,
+  NavigationState,
 } from 'react-navigation';
-import HamburgerMenu from "./HamburgerMenu";
+import HamburgerMenu from './HamburgerMenu';
+
 export interface Props {
-    title: string
-    navigation: NavigationScreenProp<NavigationState, NavigationParams>;
-
+  title: string;
+  navigation: NavigationScreenProp<NavigationState, NavigationParams>;
 }
-const HandyHeader = ({ title, navigation }: Props) => {
-    return (
-        <Header
-            containerStyle={{ height: 50, backgroundColor: '#078ca9', marginBottom: 10, paddingTop: 0 }}
 
-            leftComponent={<HamburgerMenu navigation={navigation} />}
-            centerComponent={{
-                text: title,
-                style: { color: "#f2f2f2", fontWeight: "bold", fontFamily: 'Cochin', fontSize: 25, backgroundColor: '#078ca9' }
-            }}
-            statusBarProps={{ barStyle: "light-content" }}
-
-        />
-    );
+const HandyHeader = ({title, navigation}: Props) => {
+  return (
+    <Header
+      containerStyle={{
+        height: 50,
+        backgroundColor: '#078ca9',
+        marginBottom: 10,
+        paddingTop: 0,
+      }}
+      leftComponent={<HamburgerMenu navigation={navigation} />}
+      centerComponent={{
+        text: title,
+        style: {
+          color: '#f2f2f2',
+          fontWeight: 'bold',
+          fontFamily: 'Cochin',
+          fontSize: 25,
+          backgroundColor: '#078ca9',
+        },
+      }}
+      statusBarProps={{barStyle: 'light-content'}}
+    />
+  );
 };
 
 export default HandyHeader;
